6ce0c7e1c32a7957136daf258e8f2ea7dfbfeb23
lehungio <me@lehungio.com>
2022-12-28 22:55:11 +0900

    reactjs - state and lifecycle - stateless and stateful components

--- reactjs/state-and-lifecycle/src/index.js
+++ reactjs/state-and-lifecycle/src/index.js
@@ -4,26 +4,62 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-  
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  root.render(element);
-}
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-setInterval(tick, 1000);
+// function Clock(props) {
+//   return (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {props.date.toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+// }
+// You can convert a function component like Clock to a class in five steps:
+// Create an ES6 class, with the same name, that extends React.Component.
+// Add a single empty method to it called render().
+// Move the body of the function into the render() method.
+// Replace props with this.props in the render() body.
+// Delete the remaining empty function declaration.
+// class Clock extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {date: new Date()};
+//   }
+
+//   componentDidMount() {
+//     this.timerID = setInterval(
+//       () => this.tick(),
+//       1000
+//     );
+//   }
+
+//   componentWillUnmount() {
+//     clearInterval(this.timerID);
+//   }
+
+//   tick() {
+//     this.setState({
+//       date: new Date()
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello, world!</h1>
+//         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+//       </div>
+//     );
+//   }
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<Clock />)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
