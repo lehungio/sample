462a749413dca8a5459732a8dc23cf4efc140b01
lehungio <me@lehungio.com>
2022-12-31 01:42:18 +0900

    reactjs - condition rendering - Inline logical / If-Else / Prevent rendering

--- reactjs/conditional-rendering/src/App.js
+++ reactjs/conditional-rendering/src/App.js
@@ -1,9 +1,151 @@
 import React from 'react';
 import './App.css';
 
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>;
+}
+
+function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>;
+}
+
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  
+  if (isLoggedIn) {
+    return <UserGreeting />;
+  }
+  return <GuestGreeting />;
+}
+
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  );
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  );
+}
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = {isLoggedIn: false};
+
+  }
+
+  handleLoginClick() {
+    this.setState({isLoggedIn: true});
+  }
+
+  handleLogoutClick() {
+    this.setState({isLoggedIn: false});
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
+    
+    if (isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />;
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />;
+    }
+
+    return (
+      <div>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
+
+        <div>
+          <p>Inline If-Else with Conditional Operator:</p> 
+          The user is <b>{isLoggedIn ? 'currently' : 'not'}</b> logged in.
+        </div>
+      </div>
+    );
+  }
+}
+
+function Mailbox(props) {
+  const unreadMessages = props.unreadMessages;
+  return (
+    <div>
+      <h1>Hello!</h1>
+      {unreadMessages.length > 0 &&
+        <h2>
+          You have {unreadMessages.length} unread messages.
+        </h2>
+      }
+    </div>
+  );
+}
+
+const messages = ['React', 'Re: React', 'Re:Re: React'];
+
+// Preventing Component from Rendering
+// https://reactjs.org/docs/conditional-rendering.html#preventing-component-from-rendering
+function WarningBanner(props) {
+  if (!props.warn) {
+    return null;
+  }
+
+  return (
+    <div className="warning">
+      Warning!
+    </div>
+  );
+}
+
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {showWarning: false};
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+  }
+
+  handleToggleClick() {
+    this.setState(state => ({
+      showWarning: !state.showWarning
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Preventing Component from Rendering:</p>
+        <WarningBanner warn={this.state.showWarning} />
+        <button onClick={this.handleToggleClick}>
+          {this.state.showWarning ? 'Hide' : 'Show'}
+        </button>
+      </div>
+    );
+  }
+}
+
 function App() {
   return (
     <div>
+      <Greeting isLoggedIn={false} />
+      <Greeting isLoggedIn={true} />
+
+      {/* Stateful component for LoginControl */}
+      <LoginControl />
+
+      {/* Inline If with Logical && Operator */}
+      {/* https://reactjs.org/docs/conditional-rendering.html#inline-if-with-logical--operator */}
+      <Mailbox unreadMessages={messages} />
+
+      {/* <p>Preventing Component from Rendering:</p> */}
+      <Page />
     </div>
   );
 }
