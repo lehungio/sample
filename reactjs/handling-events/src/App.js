611148e82852bdc04b5a10662610541ed8144646
lehungio <me@lehungio.com>
2022-12-30 12:12:22 +0900

    reactjs - handling events - using this in JSX callbacks

--- reactjs/handling-events/src/App.js
+++ reactjs/handling-events/src/App.js
@@ -1,9 +1,54 @@
+import React from 'react';
 import './App.css';
+
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
+}
+
+class LoggingButton extends React.Component {
+  // This syntax ensures `this` is bound within handleClick.
+  handleClick = () => {
+    console.log('this is:', this);
+  };
+  
+  render() {
+    return (
+      <>
+      <button onClick={this.handleClick}>
+        Click me
+      </button>
+      <button onClick={() => this.handleClick()}>
+          Click me
+      </button></>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Toggle />
+      <LoggingButton />
     </div>
   );
 }
