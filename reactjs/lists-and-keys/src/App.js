868c2fa17a2569c8a5f524c8842b918ce1bafb27
lehungio <me@lehungio.com>
2022-12-31 22:40:44 +0900

    reacjs - lists and keys - keys must only be unique among siblings

--- reactjs/lists-and-keys/src/App.js
+++ reactjs/lists-and-keys/src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import './App.css';
 
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    <li key={number.toString()}>
+      {number}
+    </li>
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
 function App() {
+  const numbers = [1, 2, 3, 4, 5];
+
   return (
     <div>
+      <NumberList numbers={numbers} />
     </div>
   );
 }
